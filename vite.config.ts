48382fa0a974776b25aebd18c2ee4e2e4a83477d
lettiegogo <lettiegogo@users.noreply.github.com>
2026-04-16 11:17:03 +0800

    fix: vite base config for dev mode

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  base: '/-retarget-config-2.0/',
+export default defineConfig(({ command }) => ({
+  base: command === 'build' ? '/-retarget-config-2.0/' : '/',
   plugins: [react()],
-})
+}))
